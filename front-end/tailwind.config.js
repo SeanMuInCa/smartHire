bd897fbc82ce92aee083c058d7d08067cf051996
Sean <fengying1028@hotmail.com>
2025-02-24 14:17:39 -0600

    tailwind css components

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import { mtConfig } from "@material-tailwind/react"
 module.exports = {
-  content: ["./src/**/*.{html,cjs,ts,tsx,jsx}"],
+  content: ["./src/**/*.{html,cjs,ts,tsx,jsx}","./node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [mtConfig],
 }
